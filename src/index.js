218475d8e811aca9cf7edc68cf334fba2f7712fb
Jayanth <jayanth.kurmeti@gmail.com>
2025-04-11 13:46:42 +0530

    utils initialized and derived

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+
+
+
+
+
+
+
+
+dotenv.config({path: './env'})
+
+
+
+connectDB()
+
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server running on port ${process.env.PORT}`);
+  })
+  app.on(('error',(e)=>{
+    console.log('Error connecting to MongoDB:', e);
+    
+  }))
+})
+
+.catch((e)=>{
+  console.log('Error connecting to MongoDB:', e);
+})
+
+
+
+
+
+
+/*
+const app = express();
+
+( async() => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on('error',(e)=>{
+      console.log('Error connecting to MongoDB');
+      throw error;
+      
+    })
+    app.listen(process.env.PORT,()=>{
+      console.log(`Server running on port ${process.env.PORT}`);
+      
+    })
+  } catch (error) {
+    console.error(' Error:' ,error); }
+} ) ()
+    
+*/
